241cb8c54709ba35bac19cf2e86d05d7b4bf90e6
GuiLeoni14 <guileone167@gmail.com>
2022-02-06 12:29:16 -0200

    add Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,19 @@
+import Container from '../../layout/Container';
+import Particles from 'react-tsparticles';
+import Typist from 'react-typist';
 export default function Home() {
-    return <h1>Home</h1>;
+    return (
+        <section className="s-home">
+            <Container>
+                <div className="main-home">
+                    <Particles id="tsparticles" />
+                    <Typist>
+                        <span> First Sentence </span>
+                        <Typist.Backspace count={8} delay={200} />
+                        <span> Phrase </span>
+                    </Typist>
+                </div>
+            </Container>
+        </section>
+    );
 }
