e1b724bf172a3653dfb67c26f1ff290eba712233
GuiLeoni14 <guileone167@gmail.com>
2022-02-06 22:39:24 -0200

    adicionando aos animate

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,22 +2,25 @@ import Container from '../../layout/Container';
 import Typist from 'react-typist';
 import Stars from '../../components/Particles/Stars';
 import './styles.scss';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import FrasesProvider from '../../context/FrasesProvider';
 import Frase from './Frase';
-import AmongUs from '../../components/Particles/AmongUs';
-import * as Switch from '@radix-ui/react-switch';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import Switch_All from '../../components/Switch';
 export default function Home() {
+    AOS.init({
+        once: false,
+    });
     const [modeLight, setModeLight] = useState(true);
+    const home = useRef(null);
+    console.log(home.current);
     return (
         <FrasesProvider>
-            <section className={modeLight ? `light s_home` : `s_home`}>
+            <section className={modeLight ? `light s_home` : `s_home`} ref={home}>
                 <Container>
-                    <div className="main-home">
-                        <Typist startDelay={800}>
-                            <h1>Inspire-se, e transforme o Mundo!</h1>
-                        </Typist>
+                    <div className="main-home" data-aos="fade-up">
+                        <h1 data-aos="zoom-in">Inspire-se, e transforme o Mundo!</h1>
                         <Stars />
                         <Frase />
                     </div>
