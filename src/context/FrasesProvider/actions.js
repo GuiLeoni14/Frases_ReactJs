cc116e1c34ffbe0c060693273ed9401f061d5cbc
GuiLeoni14 <guileone167@gmail.com>
2022-02-14 20:48:07 -0200

    alterando modo de busca para frases.js

--- src/context/FrasesProvider/actions.js
+++ src/context/FrasesProvider/actions.js
@@ -1,8 +1,8 @@
 import * as types from './types';
+import { frases } from '../../../frases';
 export const loadFrases = async (dispatch) => {
     dispatch({ type: types.FRASES_LOADING });
-    const response = await fetch('http://localhost:5000/frases');
-    const data = await response.json();
+    const data = frases;
     return () => dispatch({ type: types.FRASES_SUCCESS, payload: data });
 };
 export const searchOneFrases = async (dispatch, frases) => {
